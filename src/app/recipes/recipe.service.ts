a0be403b2bb1e0cec8bb06e86e721f6b90e333b5
PC <TamaraVu>
2021-01-04 23:16:01 +0100

    First commit

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,48 @@
+import {Recipe} from './recipe.model';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://media.self.com/photos/5f1eef2914b005b8d8eba4d0/4:3/w_384/30-Minute-Roasted-Vegetable-Tacos-with-Chimichurri-BIG-flavor-satisfying-HEALTHY-vegan-glutenfree-plantbased-tacos-chimichurri-cauliflower-minimalistbaker-recipe-6.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://media.self.com/photos/5f1eef2914b005b8d8eba4d0/4:3/w_384/30-Minute-Roasted-Vegetable-Tacos-with-Chimichurri-BIG-flavor-satisfying-HEALTHY-vegan-glutenfree-plantbased-tacos-chimichurri-cauliflower-minimalistbaker-recipe-6.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) {
+  }
+
+  // tslint:disable-next-line:typedef
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  // tslint:disable-next-line:typedef
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  // tslint:disable-next-line:typedef
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
